cdb82c985fda52c2dd5aeaca350b2105f0fb876a
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2023-11-16 10:03:02 +0000

    scripts: Import OEM Changes
    Signed-off-by: LMAO-armv8 <chaitanya4g9@sasi.ac.in>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -403,5 +403,15 @@ if [ -n "${CONFIG_KALLSYMS}" ]; then
 	fi
 fi
 
+if [ -n "${CONFIG_CRYPTO_FIPS}" ]; then
+	echo '  FIPS : Generating hmac of crypto and updating vmlinux... '
+	PYTHONDONTWRITEBYTECODE=0 "${srctree}/scripts/crypto/fips_crypto_integrity.py" "${objtree}/vmlinux" "${CROSS_COMPILE}readelf"
+fi
+
+if [ -n "${CONFIG_EXYNOS_FMP_FIPS}" ]; then
+    echo '  FIPS : Generating hmac of fmp and updating vmlinux... '
+	PYTHONDONTWRITEBYTECODE=0 "${srctree}/scripts/fmp/fips_fmp_integrity.py" "${objtree}/vmlinux" "${CROSS_COMPILE}readelf"
+fi
+
 # We made a new kernel - delete old version file
 rm -f .old_version
