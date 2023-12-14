fidoedidoe/android_kernel_samsung_universal8895

devfreq_boost: Don't apply boost while watching videos
Bus frequency needed for up to 1080p60 decoding isn't high. Applying
boost will cause high power consumption

Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

drivers/devfreq/devfreq_boost.c
@@ -30,6 +30,12 @@ struct df_boost_drv {
 };
 
 static struct df_boost_drv *df_boost_drv_g __read_mostly;
+static int disable_boost = 0;
+
+void disable_devfreq_boost(int disable)
+{
+	disable_boost = disable;
+}
 
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
@@ -53,6 +59,9 @@ void devfreq_boost_kick(enum df_device device)
 		return;
 
 	if (!d->screen_awake)
+		return;
+
+	if (disable_boost)
 		return;
 
 	__devfreq_boost_kick(d->devices + device);
@@ -89,6 +98,9 @@ void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 		return;
 
 	if (!d->screen_awake)
+		return;
+
+	if (disable_boost)
 		return;
 
 	__devfreq_boost_kick_max(d->devices + device, duration_ms);

LMAO-armv8/kernel_samsung_m31

drivers/devfreq/devfreq_boost.c
@@ -52,6 +52,12 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 	BOOST_DEV_INIT(df_boost_drv_g, DEVFREQ_EXYNOS_MIF,
 		       CONFIG_DEVFREQ_EXYNOS_MIF_BOOST_FREQ)
 };
+static int disable_boost = 0;
+
+void disable_devfreq_video_boost(int disable)
+{
+	disable_boost = disable;
+}
 
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
@@ -67,6 +73,9 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 void devfreq_boost_kick(enum df_device device)
 {
 	struct df_boost_drv *d = &df_boost_drv_g;
+
+	if (disable_boost)
+		return;
 
 	__devfreq_boost_kick(d->devices + device);
 }