5f624c9526754eef09297164c578ee864b2a9d96
ghazzor <kartikhis8o@gmail.com>
2023-12-15 00:44:04 +0530

    Add Workflow that builds with GCC 11

---
+++ auto_workflow_gcc11.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#export CC=clang
+export CROSS_COMPILE=$PWD/toolchain/gcc/bin/aarch64-none-linux-gnu-
+export LD=ld.lld
+#export CLANG_TRIPLE=~/android/toolchain/gcc/bin/aarch64-linux-gnu-
+export AR=llvm-ar
+export NM=llvm-nm
+export OBJCOPY=llvm-objcopy
+export OBJDUMP=llvm-objdump
+export STRIP=llvm-strip
+make clean && make distclean
+
+export ARCH=arm64
+export PLATFORM_VERSION=12
+export ANDROID_MAJOR_VERSION=s
+
+make LD=ld.lld ARCH=arm64 KCFLAGS=-w AR=llvm-ar NM=llvm-nm LLVM_NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip LLVM_AR=llvm-ar LLVM_DIS=llvm-dis CONFIG_SECTION_MISMATCH_WARN_ONLY=y exynos9610-m31nsxx_gcc_defconfig
+make LD=ld.lld ARCH=arm64 KCFLAGS=-w AR=llvm-ar NM=llvm-nm LLVM_NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip LLVM_AR=llvm-ar LLVM_DIS=llvm-dis CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
+
+
+rm -rf AIK/Image
+rm -rf AIK/dtb
+rm -rf AIK/dtbo
+cp -r arch/arm64/boot/Image AIK/Image
+cp -r arch/arm64/boot/dtb_exynos.img AIK/dtb
+cp -r arch/arm64/boot/dtbo_exynos.img AIK/dtbo
+cd AIK
+rm -rf LMAO_kernel_m31.zip
+rm -rf LMAO_kernel_m31s.zip
+zip -r9 LMAO_kernel_m31.zip * -x .git README.md *placeholder
+cd ..
